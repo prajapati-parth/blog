dabad02b8e541d385995661324eb0935de771262
Parth Prajapati <parth17prajapati@gmail.com>
2019-09-20 10:05:39 +0530

    Interchanged profile and blog page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,85 +1,54 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import get from 'lodash/get'
-import sortBy from 'lodash/sortBy'
 import Helmet from 'react-helmet'
-import LazyLoad from 'react-lazyload'
+import get from 'lodash/get'
 
-import SitePost from '../components/SitePost'
+import { siteMetadata } from '../../gatsby-config'
+import ProfileSideBar from '../components/ProfileSidebar'
+import ProfileContent from '../components/ProfileContent'
+import withGaTracker from '../hocs/withGaTracker'
 
-class BlogIndex extends React.Component {
+class Profile extends React.Component {
   render() {
-    const pageLinks = []
-    const site = get(this, 'props.data.site.siteMetadata')
-    const posts = get(this, 'props.data.remark.posts')
-
-    const sortedPosts = sortBy(posts, post =>
-      get(post, 'post.frontmatter.date')
-    ).reverse()
-
-    sortedPosts.forEach((data, i) => {
-      const layout = get(data, 'post.frontmatter.layout')
-      const path = get(data, 'post.path')
-      if (layout === 'post' && path !== '/404/') {
-        pageLinks.push(
-          <LazyLoad height={500} offset={500} once={true} key={i}>
-            <SitePost data={data.post} site={site} isIndex={true} key={i} />
-          </LazyLoad>
-        )
-      }
-    })
+    const title = 'Profile'
 
     return (
       <div>
         <Helmet
-          title={get(site, 'title')}
+          title={`${title} | ${get(siteMetadata, 'title')}`}
           meta={[
             { name: 'twitter:card', content: 'summary' },
-            { name: 'twitter:site', content: `@${get(site, 'twitter')}` },
-            { property: 'og:title', content: get(site, 'title') },
+            {
+              name: 'twitter:site',
+              content: `@${get(siteMetadata, 'twitter')}`,
+            },
+            { property: 'og:title', content: title },
             { property: 'og:type', content: 'website' },
-            { property: 'og:description', content: get(site, 'description') },
-            { property: 'og:url', content: get(site, 'url') },
+            {
+              property: 'og:description',
+              content: get(siteMetadata, 'description'),
+            },
+            {
+              property: 'og:url',
+              content: `${get(siteMetadata, 'siteUrl')}/profile`,
+            },
             {
               property: 'og:image',
-              content: `${get(site, 'url')}/img/profile.jpg`,
+              content: `https://avatars1.githubusercontent.com/u/17194534`,
             },
           ]}
         />
-        {pageLinks}
+
+        <div className="row">
+          <div className="col-xs-12 col-sm-4">
+            <ProfileSideBar />
+          </div>
+          <div className="col-xs-12 col-sm-8">
+            <ProfileContent />
+          </div>
+        </div>
       </div>
     )
   }
 }
 
-export default BlogIndex
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        url: siteUrl
-        author
-        twitter
-        adsense
-      }
-    }
-    remark: allMarkdownRemark {
-      posts: edges {
-        post: node {
-          html
-          excerpt(pruneLength: 250)
-          frontmatter {
-            layout
-            title
-            path
-            categories
-            date(formatString: "DD MMM, YYYY")
-          }
-        }
-      }
-    }
-  }
-`
+export default withGaTracker(Profile)
