ce420b628b3698859428205a0c1c0c2130bcecbd
Parth Prajapati <prajapati-parth@users.noreply.github.com>
2019-11-14 23:19:23 +0530

    Updated profile image

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -33,7 +33,7 @@ class Profile extends React.Component {
             },
             {
               property: 'og:image',
-              content: `https://avatars1.githubusercontent.com/u/17194534`,
+              content: `https://avatars6.githubusercontent.com/u/17194534`,
             },
           ]}
         />
